982d311172500b896772c12f69a9f2719e7644b0
kaskadddd <forfantokaskad@gmail.com>
2025-09-20 13:32:50 +0300

    hw

--- server.py
+++ server.py
@@ -1,10 +1,37 @@
 import asyncio 
 import random 
 import json
-
+import sqlite3
+conn=sqlite3.connect("dice.db")
+cursor=conn.cursor()
+create="""
+CREATE TABLE IF NOT EXISTS login_data(
+id INTEGER PRIMARY KEY AUTOINCREMENT
+login TEXT NOT NULL
+passworld TEXT NOT NULL,
+passord TEXT NOT NULL
+)
+"""
 HOST,PORT="0.0.0.0",8080
-
+cursor.execute(create_table)
 clients=[]
+async def add_user(login,password):
+    query="""
+    SELECT login
+    FROM login_data
+    WHERE login=?
+    """
+    cursor.execute(query,(login))
+    rows=cursor.fetchall()
+    if not rows:
+        query="""
+        INSERT INTO login_data(login,password)VALUES(?,?)
+        """
+        cursor.execute(query,(login,password))
+        conn.commit
+        return json.dumps({"respond":"success"})
+    else:
+        return json.dumps({"respond":"user alredy exists"})
 async def broadcast(msg):
     for writer, _ in clients:
         try:
@@ -31,6 +58,11 @@ async def client_handler(reader,writer):
             if cmd=="roll":
                 roll=random.randint(1,6)
                 await broadcast(f"{name} got: {roll}")
+            elif msg.get("cmd").lower()=="add_user":
+                login,password=msg.get("login"),msg.get("password")
+                respond=await add_user(login,password)
+                writer.write(respond.encode())
+                await writer.drain
     except:
         pass
 async def main():
